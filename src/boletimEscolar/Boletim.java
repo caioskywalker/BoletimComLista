65fcda8f8394689a60c0f4096b8669778d4fc5a3
Caio Felipe <caiof.farias@outlook.com>
2023-03-22 11:35:54 -0300

    Atualizado com condicionais para validar cada caso

--- src/boletimEscolar/Boletim.java
+++ src/boletimEscolar/Boletim.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 
 
+
 public class Boletim {
 
 	public static void main(String[] args) {
@@ -10,9 +11,6 @@ public class Boletim {
 		String resposta;
 		Scanner entrada = new Scanner(System.in);
 		Notas listaNotas = new Notas();
-		String notaaa ;
-		
-	
 		
 		
 		System.out.println("Voce deseja saber se esta aprovado ou reprovado? Digite 'Sim', se quiser.");
@@ -20,14 +18,58 @@ public class Boletim {
 		
 		if(resposta.equalsIgnoreCase("Sim")) {
 		
-			System.out.println("Entao vamos comecar! \n ");
-			System.out.println("Digite sua primeira nota. Se a nota nao for um numero inteiro, digite com virgula :)... \nExemplo: 8,5  \n");
-			listaNotas.notas.add(0, entrada.nextDouble());
-			System.out.printf("A primeira nota digitada foi... " + listaNotas.notas.get(0));
+			System.out.println("Entao vamos comecar! Você digitará 4 notas. \n ");
+			
+			for(int i = 0 ; i < 4 ; i++) {
+			System.out.println("Digite sua nota. Se a nota nao for um numero inteiro, digite com virgula :)... \nExemplo: 8,5  \n");
+			listaNotas.notas.add(i, entrada.nextDouble());
+			System.out.printf("A nota digitada foi... " + listaNotas.notas.get(i) + ". \n");
+			}
 		}
+		
+		
 		else {
-			System.out.println("Fechando o programa...");
+			System.out.println("Programa finalizado...");
+				
 		}
+		
+		
+		if(resposta.equalsIgnoreCase("Sim")) {
+		
+			entrada.close();
+		
+			System.out.println("Agora vamos calcular sua media e saber se voce esta aprovado... \n");
+			System.out.println("Suas notas são: \n " + listaNotas.notas + ". \n");
+		
+			Double nota1 = listaNotas.notas.get(0);
+			Double nota2 = listaNotas.notas.get(1);
+			Double nota3 = listaNotas.notas.get(2);
+			Double nota4 = listaNotas.notas.get(3);
+			
+			Double soma = nota1 + nota2 + nota3 + nota4;
+			Double mediaSoma = soma /4;
+	
+			
+			if(mediaSoma >= 7) {
+				System.out.printf("Sua média e: " + mediaSoma + ". Voce está aprovado" );
+			}
+			
+			else if(mediaSoma >= 5  ) {
+				System.out.println("Sua média e: " + mediaSoma + ". Voce está de recuperacao." );
+				
+			}
+			else {
+				System.out.println("Sua media e: " + mediaSoma + ". Voce esta reprovado.");
+			}
+		}
+				
+		
+		
+		
+		
+		
+		
+		
 		
 			
 			
